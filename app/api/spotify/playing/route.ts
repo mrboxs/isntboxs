5a185d806bea94e8e5b5c56e8a11308257718bfb
mrboxs <rizkywanda200155@gmail.com>
2024-07-26 21:23:16 +0700

    create spotify api & now playing

---
+++ app/api/spotify/playing/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server"
+
+import { getNowPlaying } from "@/lib/spotify"
+
+export const revalidate = 30
+export async function GET(request: Request) {
+  try {
+    const response = await getNowPlaying()
+    if (response.status === 204) return NextResponse.json({ isPlaying: false })
+
+    const json: CurrentlyPlayingResponse = await response.json()
+    const track: CurrentlyPlayingItems = {
+      isPlaying: json.is_playing,
+      title: json.item.name,
+      album: json.item.album.name,
+      albumImage: json.item.album.images[0],
+      artist: json.item.artists.map((artist) => ({
+        name: artist.name,
+        id: artist.id,
+        url: `https://open.spotify.com/artist/${artist.id}`,
+      })),
+      songId: json.item.id,
+      songUrl: `https://open.spotify.com/track/${json.item.id}`,
+      timestamp: json.timestamp,
+      progress_ms: json.progress_ms,
+      duration_ms: json.item.duration_ms,
+    }
+
+    return NextResponse.json(track)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    )
+  }
+}
---
+++ types/spotify/now-playing.ts
@@ -0,0 +1,56 @@
+interface CurrentlyPlayingResponse {
+  progress_ms: number
+  timestamp: number
+  is_playing: boolean
+  item: TrackResponseItems
+  actions: {
+    disallows: {
+      pausing?: boolean
+    }
+  }
+}
+
+interface TrackResponseItems {
+  duration_ms: number
+  album: AlbumResponse
+  artists: ArtistResponse[]
+  id: string
+  name: string
+}
+
+interface AlbumResponse {
+  name: string
+  images: AlbumResponseImage[]
+}
+
+interface AlbumResponseImage {
+  url: string
+  height: number
+  width: number
+}
+
+interface ArtistResponse {
+  id: string
+  name: string
+}
+
+interface BaseTrack {
+  songId: string
+  title: string
+  album: string
+  albumImage: AlbumResponseImage
+  artist: SpotifyArtist[]
+  songUrl: string
+  timestamp: number
+  progress_ms: number
+  duration_ms: number
+}
+
+interface SpotifyArtist {
+  id: string
+  name: string
+}
+
+interface CurrentlyPlayingItems extends BaseTrack {
+  isPlaying: boolean
+}
